73033a0296892c4e8d6fca2826bdac4cad891d44
CeGisele <cegisele@hotmail.com>
2014-05-16 21:36:45 -0300

    Agregada conexion para la base de datos

---
+++ Datos/ConexionBD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Datos
+{
+    class ConexionBD
+    {
+        SqlConnection sqlconn;
+        //public SqlConnection Sqlconn
+        //{
+        //    get { return sqlconn; }
+        //    set { sqlconn = value; }
+        //}
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conectar();
+        }
+        public bool conectar()
+        {
+            string conexion = cadenaDeConexion();
+            sqlconn = new SqlConnection(conexion);
+            sqlconn.Open();
+            return (sqlconn.State == ConnectionState.Open);
+        }
+        public String cadenaDeConexion()
+        {
+            SqlConnectionStringBuilder miConexion = new SqlConnectionStringBuilder();
+            miConexion.DataSource = "CECILIA\\SQLEXPRESS"; // PONER EL NOMBRE DEL SERVIDOR PROPIO
+            miConexion.InitialCatalog = " ";//PONER NOMBRE DE LA BD!!!
+            miConexion.IntegratedSecurity = true;
+            return miConexion.ConnectionString;
+        }
+    }
+}
