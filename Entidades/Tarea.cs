3fba5d0d8a1c888eea52112bb65897d877dadcb9
AgoFig <agostinafigueredo@gmail.com>
2014-05-20 08:37:36 -0300

    Creados en Entidades las clases Carpeta y Tarea

---
+++ Entidades/Tarea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entidades
+{
+    public class Tarea
+    {
+        public int IdCarpeta { set; get; }
+        public int IdUsuario { set; get; }
+        public string Nombre { set; get; }
+        public string Descripcion { set; get; }
+        public DateTime fecha { set; get; }
+        public int Prioridad { set; get; }
+        public int Estado { set; get; }
+    }
+}
